8c960b185d8f81128ea04895d2db808a4e06473b
tushar27 <tushar.27041994@gmail.com>
2024-11-05 20:35:43 +0530

    basic test

--- Home.py
+++ Home.py
@@ -22,3 +22,117 @@ from fpdf import FPDF
 
 from crewai import Agent, Task, Crew, Process
 from langchain_openai import ChatOpenAI
+
+#Import from SRC
+from src.components.navigation import page_config, custom_style, footer
+
+# Setting up Llama3 via Ollama server @http://localhost:11434/v1 
+os.environ["OPENAI_API_KEY"] = "NA"
+
+llm = ChatOpenAI(
+    model="crewai-llama3",
+    base_url="http://localhost:11434/v1"
+)
+
+search_tool = DuckDuckGoSearchRun()
+
+###################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    # Streamlit Page Setup
+    page_config("DocLib", "🤖", "wide") 
+    custom_style()
+    # st.sidebar.image('./src/logo.png')
+
+    st.title("🤖DocLib🤖")
+    st.markdown(
+        '''
+        <style>
+            div.block-container{padding-top:0px;}
+            font-family: 'Roboto', sans-serif; /* Add Roboto font */
+            color: blue; /* Make the text blue */
+        </style>
+        ''',
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        ### DocLib help you find specialist nearby while suggestion home remedies for your health issues.
+        """
+    )
+
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+    
+    col1, col2 = st.columns(2)
+    
+    # Input for blog topic
+    with col1:
+        symptoms = st.text_input("Enter current symptoms:")
+
+    # Dropdown for selecting the type of content  
+    with col2:
+        intensity = st.selectbox(
+            "Select the intensity of symptoms:", 
+            ["Mild", "Moderate", "Severe"]
+        )
+
+    if st.button("Get Advice"):
+        if symptoms:
+            # blog_crew = BlogCrew(blog_topic, content_type)
+            # result = blog_crew.run()
+            st.write("Test Advice")
+        else:
+            st.error("Please enter your symptoms first.")
+
+
+if __name__ == "__main__":
+    main()
+    with st.sidebar:
+        footer()
